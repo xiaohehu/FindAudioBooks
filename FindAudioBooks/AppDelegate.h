006e02acc02ad7ce371fc03b209e47f9e600370c
Xiaohe Hu <huxiaohe113@gmail.com>
2018-04-15 17:03:42 -0400

    Initial Commit

---
+++ FindAudioBooks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FindAudioBooks
+//
+//  Created by Xiaohe Hu on 4/15/18.
+//  Copyright © 2018 com.xiaohehu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FindAudioBooks/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FindAudioBooks
+//
+//  Created by Xiaohe Hu on 4/15/18.
+//  Copyright © 2018 com.xiaohehu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
