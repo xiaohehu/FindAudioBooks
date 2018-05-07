e8c052857126eb05cb5cec7e61e7847f400b6926
Xiaohe Hu <huxiaohe113@gmail.com>
2018-05-07 00:40:32 -0400

    Added DBManger class and sql flie

---
+++ FindAudioBooks/DBManager.h
@@ -0,0 +1,27 @@
+//
+//  DBManager.h
+//  FindAudioBooks
+//
+//  Created by Xiaohe Hu on 5/6/18.
+//  Copyright © 2018 com.xiaohehu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <sqlite3.h>
+
+@interface DBManager : NSObject
+
+@property (nonatomic, strong) NSString *documentsDirectory;
+@property (nonatomic, strong) NSString *databaseFilename;
+@property (nonatomic, strong) NSMutableArray *arrResults;
+@property (nonatomic, strong) NSMutableArray *arrColumnNames;
+@property (nonatomic) int affectedRows;
+@property (nonatomic) long long lastInsertedRowID;
+
+- (void)runQuery:(const char *)query isQueryExecutable:(BOOL)queryExecutable;
+- (instancetype)initWithDatabaseFilename:(NSString *)dbFilename;
+- (void)copyDatabaseIntoDocumentsDirectory;
+-(NSArray *)loadDataFromDB:(NSString *)query;
+-(void)executeQuery:(NSString *)query;
+
+@end
